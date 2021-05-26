7b11ceb37b11bb61428a6d5cadca86850a0b0440
root <tjhacker@purdue.edu>
2021-05-26 13:44:27 -0400

    Added start to etherate_service

--- paper/hub/doit.sh
+++ paper/hub/doit.sh
@@ -8,7 +8,7 @@ sh -x ./minion_create -m vxlan1  -i eth2 -a 192.100.44.21 -v 31 -r hub -n ab -s 
 sh -x ./minion_create -m vxlan2  -i eth2 -a 192.100.46.22 -v 41 -r hub -n ab -s Mars 
 
 #sh -x ./boot_service -m vxlan0 -r spoke -i eth1
-sh -x ./etherate_service -m etherate -r hub -b ovs-br1 
+sh -x ./etherate_service start -m etherate -r hub -b ovs-br1 
 #sh -x ./boot_service_hub -m boot -r hub -b ovs-br1 -I 192.7.7.4 -D 192.7.7.4
 #sh -x ./boot_service_hub -m boot1 -r hub -b ovs-br2 -I 192.10.7.4 -D 192.10.7.4
 sh -x ./boot_service -m boot -r hub -b ovs-br1 -I 192.7.7.4 -D 192.7.7.4 -R 192.7.7.4 -C 192.7.7.4 -B 192.7.7.12
